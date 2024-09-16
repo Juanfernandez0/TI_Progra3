c651ef06b84f94d248b84da402bb0092afb47f38
MateoMagarinos <magarinosm@udesa.edu.ar>
2024-09-16 17:42:25 -0300

    header y footer empezados

---
+++ tiprogra3/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css"
+import { link } from "react-router-dom"
+
+const Footer = () => {
+    return (
+        <div>
+            <ul>
+            <li>Ignacio Bianco</li>
+            <li>Juan Fernandez</li>
+            <li>Mateo Magariños</li>
+            </ul>
+        </div>
+    )
+}
---
+++ tiprogra3/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import "./Header.css"
+import { link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <nav>
+      <ul class="main-nav">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/favoritos">Favoritos</Link></li>
+        <li><Link to="/ver_todas">Ver todas</Link></li>
+      </ul>
+      <ul class="user">
+        <li>Nombre usuario <img src="./img/user.jpg" alt="" /></li>
+      </ul>
+    </nav>
+    )
+}
