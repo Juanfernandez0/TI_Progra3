b33991efb6715774ce0f1f48188583d918e16dfb
MateoMagarinos <magarinosm@udesa.edu.ar>
2024-09-18 20:22:06 -0300

    empezando punto 3

--- tiprogra3/src/App.js
+++ tiprogra3/src/App.js
@@ -1,11 +1,20 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <>
+    <><div>
+      <header>
+      <Header />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+      <Footer />
+      </footer>
     
-  
-    
-    </>
+    </div></>
   );
 }
 
--- tiprogra3/src/components/Header/Header.js
+++ tiprogra3/src/components/Header/Header.js
@@ -15,3 +15,5 @@ const Header = () => {
     </nav>
     )
 }
+
+export default Header
