f0dbbff13b6ff932f3d22980c069db774b17e6a3
MateoMagarinos <magarinosm@udesa.edu.ar>
2024-09-21 22:01:54 -0300

    arreglo moviecard y elimine movie

--- tiprogra3/src/components/MovieCard/MovieCard.js
+++ tiprogra3/src/components/MovieCard/MovieCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import "./MovieCard.css"
 
@@ -15,6 +16,55 @@ class MovieCard extends Component {
 
     }
 
+    componentDidMount() {
+      const storage = localStorage.getItem('favoritos')
+      if (storage !== null) {
+        const parsedArray = JSON.parse(storage)
+        const estaEnFavoritos = parsedArray.includes(this.props.movie.id)
+        this.setState({
+          esFavorito: estaEnFavoritos
+        })
+      }
+    }
+  
+    agregarFavoritos() {
+      const storage = localStorage.getItem('favoritos')
+      if (storage !== null) {
+        const parsedArray = JSON.parse(storage)
+        parsedArray.push(this.props.movie.id)
+        const stringArray = JSON.stringify(parsedArray)
+        localStorage.setItem('favoritos', stringArray)
+  
+  
+      } else {
+        const primerMovie = [this.props.movie.id]
+        const stringArray = JSON.stringify(primerMovie)
+        localStorage.setItem('favoritos', stringArray)
+      }
+      this.setState({
+        esFavorito: true
+      })
+    }
+  
+    quitarFavoritos() {
+      const storage = localStorage.getItem('favoritos')
+      const parsedArray = JSON.parse(storage)
+      const favoritosRestantes = parsedArray.filter(id => id !== this.props.movie.id)
+      const stringArray = JSON.stringify(favoritosRestantes)
+      localStorage.setItem('favoritos', stringArray)
+  
+      this.setState({
+        esFavorito: false
+      })
+  
+    }
+  
+    verDescripcion() {
+      this.setState({
+        showExtra: !this.state.showExtra
+      });
+    }
+
 
     render() {
         const { id, title, poster_path, overview } = this.props.peli;
@@ -26,10 +76,21 @@ class MovieCard extends Component {
                 <img src={`https://image.tmdb.org/t/p/w400${poster_path}`} alt={title} className="movie-img" />
               </li>
               <li>
-                <h3 className="movie-title">{title}</h3>
+               <Link to={`/movies/${id}`}>{title}</Link>
               </li>
               <li>
-                <button className="movie-description">Mas info</button>
+                <button className="descripcionBoton" onClick={() => this.verDescripcion()}>
+                  {this.state.showExtra ? "Ocultar descripción" : "Ver descripción"}
+                </button>
+                {this.state.showExtra && <p>{overview}</p>}
+                <Link to={`/movies/${id}`}><button className="detalleBoton">Ir a detalle</button></Link>
+              </li>
+              <li>
+                <Link to="/favoritos">
+                  <button onClick={this.handleClick} className="favoritosBoton">
+                   {!this.state.esFavorito ? "Agregar a favoritos" : "Quitar de favoritos"}
+                  </button>
+                </Link>
               </li>
               <li>
                 <p className="movie-rating">Rating:</p>
